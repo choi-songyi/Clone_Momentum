c4fe7c052408747c93b9ec47873a734bc96875c2
songyi <nice.dev.song@gmail.com>
2020-12-18 17:28:01 +0900

    set name load

--- main.js
+++ main.js
@@ -17,3 +17,46 @@ function TimeLoad(){
 
 TimeLoad();
 
+// name 
+const form = document.querySelector('#name-box');
+const input = document.querySelector('.name');
+const greeting = document.querySelector('.greeting');
+
+const user = "Hi";
+
+function saveName(text){
+    localStorage.setItem(user,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const inputValue = input.value;
+    sayHello(inputValue);
+    saveName(inputValue);
+}
+
+function askName(){
+    form.classList.add('show');
+    form.addEventListener('submit',handleSubmit);
+}
+
+function sayHello(text){
+    form.classList.remove('show');
+    greeting.classList.add('show');
+    greeting.innerText = `Hello ${text}!`
+}
+
+function getName(){
+    const currentUser = localStorage.getItem(user);
+    if(currentUser===null){
+        askName();
+    } else {
+        sayHello(currentUser);
+    };
+}
+
+function nameLoad(){
+    getName();
+}
+
+nameLoad();
