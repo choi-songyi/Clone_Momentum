18703506053aef9fd861c8af3f8b289b585729f2
songyi <nice.dev.song@gmail.com>
2020-12-21 11:12:50 +0900

    set bg image

--- main.js
+++ main.js
@@ -127,3 +127,25 @@ function addToList(){
 }
 
 addToList();
+
+// background
+const body = document.querySelector("body");
+const IMG_NUMBER = 5;
+function paintImage(imgNumber){
+    const image = new Image();
+    image.src = `./images/${imgNumber+1}.jpg`;
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random()*IMG_NUMBER);
+    return number;
+}
+
+function loadImage(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+loadImage();
